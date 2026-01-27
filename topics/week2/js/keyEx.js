ed33a2fb02a698b87bf58cb2f3ae7cd5ab20f641
ashmyytaa <ashmi@live.ca>
2026-01-27 16:48:44 -0500

    finishing practices

--- topics/week2/js/keyEx.js
+++ topics/week2/js/keyEx.js
@@ -1,11 +1,30 @@
 window.onload = function () {
     console.log("keys");
 
+    let speedX = 5;
+    let boxA = this.document.querySelector("#boxA");
+
+
     window.addEventListener("keydown",
-        function (e) {//body of callback function
-            console.log(e);
-            console.log(e.key);
-            console.log(e.code);
+        function (e) {
+
+            if (e.key === "ArrowRight") {
+                console.log(parseInt(boxA.style.left));
+                let currentPos = parseInt(boxA.style.left);
+                boxA.style.left = currentPos + speedX + "px"
+
+            }
+            if (e.key === "ArrowLeft") {
+                console.log(parseInt(boxA.style.left));
+                let currentPos = parseInt(boxA.style.left);
+                boxA.style.left = currentPos - speedX + "px"
+
+            }
+            //body of callback function
+            // console.log(e);
+            // console.log(e.key);
+            // console.log(e.code);
+
 
         });
 
